92da16657a0eca28d109bf040b9d3113a3c42699
CZX <chenzhixiang1992@gmail.com>
2018-03-09 17:07:26 +0800

    添加对post的view的说明

--- apps/post/views.py
+++ apps/post/views.py
@@ -32,6 +32,7 @@ class IndexView(View):
         recommend_article_list = article_list.filter(is_recommend=True, is_published=True).order_by('-created_time')  # 最新推荐
         pages, article_list = getPages(request, article_list, 10)
 
+        # 筛选出点赞、收藏、热评的前五，以时间顺序排列
         hot_like_sql = "SELECT `post_article`.`id`, `post_article`.`title`, SUM(`tools_likenum`.`like_num`) AS `like_nums` FROM `post_article` LEFT OUTER JOIN `tools_likenum` ON (`post_article`.`id` = `tools_likenum`.`object_id` AND (`tools_likenum`.`content_type_id` = 12)) WHERE `post_article`.`is_published` = True GROUP BY `post_article`.`id` ORDER BY `like_nums` DESC LIMIT 5"
         hot_favorite_sql = "SELECT `post_article`.`id`, `post_article`.`title`, COUNT( `tools_favorite`.`favorite_id` ) AS `favorite_nums` FROM post_article LEFT OUTER JOIN `tools_favorite` ON (`post_article`.`id` = `tools_favorite`.`favorite_id` AND (`tools_favorite`.`favorite_type` = 1)) WHERE `post_article`.`is_published` = TRUE GROUP BY post_article.id ORDER BY favorite_nums DESC LIMIT 5"
         hot_comment_sql = "select `post_article`.`id`, `post_article`.`title`, count( c.object_pk ) AS comment_count from post_article left join (select * from django_comments where content_type_id =12) as c on post_article.id = c.object_pk WHERE `post_article`.`is_published` = TRUE group by post_article.id order by comment_count desc  LIMIT 5"
